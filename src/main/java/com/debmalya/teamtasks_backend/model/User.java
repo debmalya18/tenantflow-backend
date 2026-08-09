9b9f4fd67be5e222294b1a93b2264e1dfadd2de4
debmalya18 <debmalyamondal772@gmail.com>
2026-08-10 02:00:50 +0530

    Add signup feature: DTO, service, controller, security config with password hashing

--- src/main/java/com/debmalya/teamtasks_backend/model/User.java
+++ src/main/java/com/debmalya/teamtasks_backend/model/User.java
@@ -16,7 +16,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name; 
+    private String name;
 
     @Column(unique = true)
     private String username;
@@ -41,6 +41,14 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {

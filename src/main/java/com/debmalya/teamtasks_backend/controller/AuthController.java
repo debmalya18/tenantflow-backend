383723e5aab4d4e5c2b07e59ff8f53989578d8ec
debmalya18 <debmalyamondal772@gmail.com>
2026-08-29 13:32:22 +0530

    Fix empty AuthController file; signup endpoint tested and working

---
+++ src/main/java/com/debmalya/teamtasks_backend/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.debmalya.teamtasks_backend.controller;
+
+import com.debmalya.teamtasks_backend.dto.SignupRequest;
+import com.debmalya.teamtasks_backend.service.AuthService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/signup")
+    public String signup(@RequestBody SignupRequest request) {
+        return authService.signup(request);
+    }
+}
